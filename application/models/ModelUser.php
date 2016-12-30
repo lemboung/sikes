649e04d8d1a5188bef3c4125f1117ac8db43ee85
lemboung <lemboung@hotmail.com>
2016-12-30 14:44:35 +0700

    fix table name
    user to pengguna

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -8,7 +8,7 @@ class ModelUser extends CI_Model {
 	 // cek keberadaan user di sistem
 	 function check_user_account($username, $password){
 		 $this->db->select('*');
-		 $this->db->from('user');
+		 $this->db->from('pengguna');
 		 $this->db->where('username', $username);
 		 $this->db->where('password', $password);
 
@@ -17,32 +17,10 @@ class ModelUser extends CI_Model {
 	// mengambil data user tertentu
 	 function get_user($id){
 		 $this->db->select('*');
-		 $this->db->from('user');
+		 $this->db->from('pengguna');
 		 $this->db->where('id_user', $id);
 
 		 return $this->db->get();
 	 }
-
-	 function select_all(){
-		 $this->db->select('*');
-		 $this->db->from('member');
-
-		 return $this->db->get();
-	 }
-	 //menambahkan data user ke table
-	 function insert_user($data){
-		 $this->db->insert('member', $data);
-
- 	 }
-
-	 function update_user($id, $data){
-		$this->db->where('id_member', $id);
-		$this->db->update('member', $data);
-	}
-
-	function delete_user($id){
-		$this->db->where('id_member', $id);
-		$this->db->delete('member');
-	}
 }
 ?>
